35e16717d6b0a251445940d52575a0a39ba7850f
Jessica Oliver <jessicaoliver@admins-MacBook-Pro.local>
2021-06-02 13:56:30 +0100

    upgrade tap-stripe

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-stripe",
-    version="1.4.4",
+    version="1.4.8",
     description="Singer.io tap for extracting data",
     author="Stitch",
     url="http://singer.io",
@@ -11,13 +11,13 @@ setup(
     py_modules=["tap_stripe"],
     install_requires=[
         "singer-python==5.5.1",
-        "stripe==2.10.1",
+        "stripe==2.56.0",
     ],
     extras_require={
         'dev': [
             'ipdb==0.11',
-            'pylint==2.1.1',
-            'astroid==2.1.0',
+            'pylint==2.7.2',
+            'astroid==2.5.1',
             'nose==1.3.7'
         ]
     },
